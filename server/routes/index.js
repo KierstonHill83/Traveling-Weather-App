20473a7c3339784d34a74a572cd848fca4ee73b2
KierstonHill83 <khill1218@gmail.com>
2015-12-21 09:29:32 -0700

    Initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var models = require('../models/index');
+
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../../client/views', 'index.html'));
+});
+
+module.exports = router;
