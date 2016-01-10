0be4ca34a0155d7b59120711b240a8b2ca625939
KierstonHill83 <khill1218@gmail.com>
2016-01-09 21:30:24 -0700

    Added service and controller for weather

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,3 +1,25 @@
-app.controller('homeController', function($scope) {
+app.controller('homeController', ['$scope', '$http', '$routeParams', '$location', 'cityService', function($scope, $http, $routeParams, $location, cityService) {
 
-});
+  $scope.city = cityService.city;
+  $scope.days = '1';
+
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  });
+
+  $scope.getWeather = function() {
+    $http.get('/api/weather/' + $scope.city + '/' + $scope.days)
+    .then(function(data) {
+
+      $scope.date = data.data[0].dt;
+      $scope.dateResults = $scope.date + 1000;
+
+      $scope.weather = data.data[0].temp.day;
+      $scope.weatherResults = Math.round((1.8 * ($scope.weather - 273)) + 32);
+
+    });
+  };
+
+  $scope.getWeather();
+
+}]);
